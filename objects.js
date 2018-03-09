63da1c16df0d1e78124c9b336e667fe588b0fc71
scoburto@gmail.com <scoburto@gmail.com>
2018-03-09 01:03:19 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,11 +2,19 @@ var playlist = new Object({Zedd: 'Papercut'})
 
 function updatePlaylist(list, artist, song) {
   var upList = new Object({});
+<<<<<<< HEAD
   Object.assign(upList, list, {[artist]: song});
+=======
+  Object.assign(upList, list, {artist: song});
+>>>>>>> 6bd4084021b754cc5ceab7167bb3ed05680de264
   return upList;
 }
 
 function removeFromPlaylist(list, artist) {
+<<<<<<< HEAD
   delete list[artist]
+=======
+  delete list.artist
+>>>>>>> 6bd4084021b754cc5ceab7167bb3ed05680de264
   return list;
 }
